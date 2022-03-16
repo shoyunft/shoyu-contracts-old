1c63ab1067d84dcdc28dabc62a340ff9d51317e7
0xMasayoshi <0xmasayoshi@protonmail.com>
2022-03-16 18:05:20 +0000

    fix: pairFor() in UniswapV2Library

--- contracts/sushiswap/uniswapv2/libraries/UniswapV2Library.sol
+++ contracts/sushiswap/uniswapv2/libraries/UniswapV2Library.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.5.0;
 
 import '../interfaces/IUniswapV2Pair.sol';
+import '../interfaces/IUniswapV2Factory.sol';
 
 import "./SafeMath.sol";
 
@@ -17,14 +18,15 @@ library UniswapV2Library {
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
-        (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
-                hex'ff',
-                factory,
-                keccak256(abi.encodePacked(token0, token1)),
-                hex'e18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303' // init code hash
-            ))));
+    function pairFor(address factory, address tokenA, address tokenB) internal view returns (address pair) {
+        pair = IUniswapV2Factory(factory).getPair(tokenA,tokenB);
+        //(address token0, address token1) = sortTokens(tokenA, tokenB);
+        //pair = address(uint(keccak256(abi.encodePacked(
+        //        hex'ff',
+        //        factory,
+        //        keccak256(abi.encodePacked(token0, token1)),
+        //        hex'e18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303' // init code hash
+        //    ))));
     }
 
     // fetches and sorts the reserves for a pair
